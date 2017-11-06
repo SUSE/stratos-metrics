a2bb43532ac8170775fc663174bc22ca978e7c44
Irfan Habib <irfan.habib@suse.com>
2017-11-06 16:22:19 +0000

    Add nginx to chart

---
+++ build/run-nginx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Checking if certificate has been supplied!"
+while : 
+do 
+    if [ -f /etc/secrets/cert.crt ]; then
+        break;
+    fi
+    sleep 1; 
+done
+echo "TLS certificate detected continuing, starting nginx."
+
+echo "Create username/password for authentication"
+htpasswd -b -c /etc/nginx/.htpasswd $USERNAME $PASSWORD
+echo "Starting nginx"
+nginx -g "daemon off;"
